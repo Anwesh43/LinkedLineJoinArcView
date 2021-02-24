53f91bc346d7426efb043b25d99915357318d206
Anwesh43 <anweshthecool0@gmail.com>
2021-02-25 01:29:05 +0530

    adding LJANode class

--- LineJoinArcView/src/main/java/com/example/linejoinarcview/LineJoinArcView.kt
+++ LineJoinArcView/src/main/java/com/example/linejoinarcview/LineJoinArcView.kt
@@ -121,4 +121,45 @@ class LineJoinArcView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LJANode(var i : Int, val state : State = State()) {
+
+        private var next : LJANode? = null
+        private var prev : LJANode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LJANode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLJANode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LJANode {
+            var curr : LJANode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
