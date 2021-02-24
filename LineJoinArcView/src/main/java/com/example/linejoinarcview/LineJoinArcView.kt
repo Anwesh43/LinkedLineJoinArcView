97514944da3ff8d2ebf640ec25d5f3d1ee19a1b0
Anwesh43 <anweshthecool0@gmail.com>
2021-02-25 01:11:28 +0530

    adding Canvas extension methods

--- LineJoinArcView/src/main/java/com/example/linejoinarcview/LineJoinArcView.kt
+++ LineJoinArcView/src/main/java/com/example/linejoinarcview/LineJoinArcView.kt
@@ -29,3 +29,31 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawLineJoinArc(w : Float, h : Float, scale : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, parts)
+    val sf2 : Float = sf.divideScale(1, parts)
+    val sf3 : Float = sf.divideScale(2, parts)
+    save()
+    translate(w / 2, h / 2)
+    save()
+    translate(-w / 2 * (1 - sf1), 0f)
+    drawLine(0f, 0f, 0f, -size, paint)
+    restore()
+    save()
+    translate(0f, h / 2 * (1 - sf2))
+    drawLine(0f, 0f, size, 0f, paint)
+    restore()
+    restore()
+}
+
+fun Canvas.drawLJANode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineJoinArc(w, h, scale, paint)
+}
